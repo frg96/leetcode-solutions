cdba3069960ae2578aadc6587e37bd7221fa6aee
Franklin George <franklingeorge96@gmail.com>
2025-12-05 18:28:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.6 MB (99.76%)

--- 0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,34 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        boolean holding = false;
-        if(prices.length == 1)
-            return profit;
+        int totalProfit = 0;
 
-        for(int i = 0; i < prices.length; ++i){
-            if(i == 0){
-                if(prices[i] < prices[i+1] && !holding){
-                    profit -= prices[i]; //buy
-                    holding = true;
-                }
+        for(int i = 1 ; i < prices.length; i++) {
+            int dayDiff = prices[i] - prices[i-1];
+
+            if(dayDiff > 0) {
+                totalProfit += dayDiff;
             }
-            else if (i == prices.length-1){
-                if(prices[i-1] <= prices[i] && holding){
-                    profit += prices[i]; //sell
-                    holding = false;
-                }
-            }
-            else{
-                if(prices[i-1] >= prices[i] && prices[i] < prices[i+1] && !holding){
-                    profit -= prices[i]; //buy
-                    holding = true;
-                }
-                else if (prices[i-1] <= prices[i] && prices[i] > prices[i+1] && holding){
-                    profit += prices[i]; //sell
-                    holding = false;
-                }
-            }            
-        } 
-        return profit;
+        }
+
+        return totalProfit;
     }
 }
